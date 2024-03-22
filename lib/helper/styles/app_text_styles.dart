d97669ca95ab328ee91c60cbbca4a1f2b24c1887
mohameedzain <128236982+mohameedzain@users.noreply.github.com>
2024-03-22 14:30:43 +0200

    Update app text styles * Removes the font family property from the other TextStyle

--- lib/helper/styles/app_text_styles.dart
+++ lib/helper/styles/app_text_styles.dart
@@ -4,36 +4,29 @@ import 'package:flutter/material.dart';
 import 'package:scanner/helper/colors.dart';
 
 class CustomTextStyle {
-  static final stylesFont400Size64 = TextStyle(
-    fontSize: 64,
-    fontWeight: FontWeight.w400,
-    color: AppColors.deepBrown,
-    fontFamily: "Pacifico",
-  );
+
   static final stylesFont500Size24 = TextStyle(
     fontSize: 22,
     fontWeight: FontWeight.w500,
     color: Colors.black,
-    fontFamily: 'Poppins',
+  
   );
 
   static final stylesFont400Size22 = TextStyle(
     fontSize: 22,
     fontWeight: FontWeight.w400,
-    color: AppColors.deepBrown,
-    fontFamily: 'Pacifico',
+
   );
   static final stylesFont300Size16 = TextStyle(
     fontSize: 16,
     fontWeight: FontWeight.w300,
     color: Colors.black,
-    fontFamily: 'Poppins',
+  
   );
   static final stylesFont600Size28 = TextStyle(
     fontSize: 28,
     fontWeight: FontWeight.w600,
-    color: AppColors.deepBrown,
-    fontFamily: "Pacifico",
+  
   );
   static final stylesFont500Size16 = TextStyle(
     fontSize: 16,
@@ -48,7 +41,7 @@ class CustomTextStyle {
   static final stylesFont500Size14 = TextStyle(
     fontSize: 14,
     fontWeight: FontWeight.w500,
-    color: AppColors.deepBrown,
+    
   );
 }
 
