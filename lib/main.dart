bc2612b16ff6260776cad13b409461e6f8f8235e
mohameedzain <128236982+mohameedzain@users.noreply.github.com>
2024-03-25 23:08:55 +0200

    Add flutter\_rating\_bar package

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:scanner/cubits/scan_cubit/scan_cubit.dart';
 import 'package:scanner/firebase_options.dart';
 import 'package:scanner/helper/routes.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:scanner/screens/products_profiles/products.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -22,7 +23,7 @@ class Scanner extends StatelessWidget {
   Widget build(BuildContext context) {
     return BlocProvider(
       create: (context) => ScanCubit(),
-      child: MaterialApp.router(
+      child: MaterialApp(
         theme: ThemeData(
           //  scaffoldBackgroundColor: AppColors.lightGrey,
           fontFamily: "ReadexPro",
@@ -34,7 +35,9 @@ class Scanner extends StatelessWidget {
         debugShowCheckedModeBanner: false,
         title: kAppTitle,
         //  routerConfig: router,_SplashScreenState
-        routerConfig: router,
+        //  routerConfig: router,
+
+        home: const CompanyProfiles(),
       ),
     );
   }
