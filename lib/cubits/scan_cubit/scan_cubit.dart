6af3f1dd50162a3d804d5cd3672731626ed1cc38
AkramMousa458 <akrammousa458@gmail.com>
2024-08-19 22:46:34 +0300

    edit version and remove baarode finder

--- lib/cubits/scan_cubit/scan_cubit.dart
+++ lib/cubits/scan_cubit/scan_cubit.dart
@@ -1,4 +1,4 @@
-import 'package:barcode_finder/barcode_finder.dart';
+// import 'package:barcode_finder/barcode_finder.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:image_picker/image_picker.dart';
@@ -36,33 +36,33 @@ class ScanCubit extends Cubit<ScanState> {
     required List<CountryBarcodeModel> countriesBarcodes,
     required List<CompanyBarcodeModel> companiesBarcodes,
   }) async {
-    emit(ScanLoading());
-    try {
-      final pickedFile =
-          await ImagePicker().pickImage(source: ImageSource.gallery);
-      if (pickedFile != null) {
-        String barcode =
-            await BarcodeFinder.scanFile(path: pickedFile.path) ?? '-1';
-        if (int.parse(barcode) != -1) {
-          try {
-            scanResult = fetchProduct(
-              productCode: barcode,
-              countriesBarcodes: countriesBarcodes,
-              companiesBarcodes: companiesBarcodes,
-            );
-            emit(ScanSuccsessGallery());
-          } on Exception catch (e) {
-            scanResult = '-1';
-            emit(ScanFailed(errMessage: e.toString()));
-          }
-        }
-      } else {
-        emit(ScanInitial());
-      }
-    } catch (e) {
-      scanResult = '';
-      emit(ScanFailed(errMessage: 'يوجد خطأ! تأكد من الصورة المراد مسحها'));
-    }
+    // emit(ScanLoading());
+    // try {
+    //   final pickedFile =
+    //       await ImagePicker().pickImage(source: ImageSource.gallery);
+    //   if (pickedFile != null) {
+    //     String barcode =
+    //         await BarcodeFinder.scanFile(path: pickedFile.path) ?? '-1';
+    //     if (int.parse(barcode) != -1) {
+    //       try {
+    //         scanResult = fetchProduct(
+    //           productCode: barcode,
+    //           countriesBarcodes: countriesBarcodes,
+    //           companiesBarcodes: companiesBarcodes,
+    //         );
+    //         emit(ScanSuccsessGallery());
+    //       } on Exception catch (e) {
+    //         scanResult = '-1';
+    //         emit(ScanFailed(errMessage: e.toString()));
+    //       }
+    //     }
+    //   } else {
+    //     emit(ScanInitial());
+    //   }
+    // } catch (e) {
+    //   scanResult = '';
+    //   emit(ScanFailed(errMessage: 'يوجد خطأ! تأكد من الصورة المراد مسحها'));
+    // }
   }
 
   void scanfromNumber({
